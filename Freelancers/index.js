d5da5f20f6bc4447d21145dc46dbfb39572b2cc8
Porter Peterson <porterpeterson@Porters-MacBook-Air.local>
2023-11-30 10:20:22 -0700

    put freelancers and occupations

---
+++ Freelancers/index.js
@@ -0,0 +1,19 @@
+const spanCount = document.querySelector("#spanCounter");
+
+const names = [
+  "Dr. Slice",
+  "Dr. Pressure",
+  "Prof. Possibility",
+  "Prof. Prism",
+  "Dr. Impulse",
+  "Prof. Spark",
+  "Dr. Wire",
+  "Prof. Goose",
+];
+
+const occupations = ["gardener", "programmer", "teacher", "gardner"];
+
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+];
